da445d605e799f6940640ad940e261201d41cdce
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2025-07-01 16:56:10 +0100

    ups

--- frontend/src/auth/register.jsx
+++ frontend/src/auth/register.jsx
@@ -94,7 +94,7 @@ const handleSubmit = async (e) => {
     <div className="min-h-screen bg-white md:bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <Toaster position="top-center" />
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-2xl md:text-3xl font-extrabold text-gray-900">
+        <h2 className="md:mt-6 text-center text-2xl md:text-3xl font-extrabold text-gray-900">
           Create your account
         </h2>
       </div>
